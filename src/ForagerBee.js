76b641f4fa8a7742707d3c3c2fc9d64d2b555ea4
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-12-05 14:05:01 -0800

    complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,16 @@
-import Bee from './Bee'
+import Bee from './Bee.js';
 
-class ForagerBee {
-  // TODO..
-};
+class ForagerBee extends Bee {
+  constructor () {
+  	super();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+  forage (treausure) {
+  	this.treasureChest.push(treausure);
+  }
+ };
 
 export default ForagerBee;
